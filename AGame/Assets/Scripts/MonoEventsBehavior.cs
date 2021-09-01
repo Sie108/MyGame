ac11ee9065696c8764148c107a67500e9a6abc49
Sierra Weber <10889154@my.uvu.edu>
2021-09-01 11:29:53 -0600

    Coding stuff

---
+++ AGame/Assets/Scripts/MonoEventsBehavior.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent, destroyEvent;
+    void Start()
+    {
+        startEvent.Invoke();        
+    }
+
+    private void OnDestroy()
+    {
+        destroyEvent.Invoke();
+    }
+}
